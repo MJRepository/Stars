56455c30739decc6c698e6b09af9aa0bf6d364f2
Mateusz Jacek <mateuszjacek@outlook.com>
2022-04-23 09:59:20 +0200

    Stars moving

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ WIDTH, HEIGHT = 1300, 700
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Stars Simulation")
 
-# STAR_COLOR = (0, 123, 132) # COLD
+# STAR_COLOR = (0, 123, 132) # COLD_COOLor
 
 
 class Star:
@@ -20,6 +20,7 @@ class Star:
         self.y = self.create_position(Star.y_pos, HEIGHT)
         self.size = random.randrange(1, 6)
         self.star_type = random.randrange(1, 4)
+        self.color = None
         Star.x_pos.append(self.x)
         Star.y_pos.append(self.y)
 
@@ -33,8 +34,20 @@ class Star:
         return xy
 
     def draw(self, win):
-        STAR_COLOR = (0, (123 - (self.star_type * random.randrange(22, 30))), (132 - (self.star_type * random.randrange(10, 30))))
-        pygame.draw.circle(win, STAR_COLOR, (self.x, self.y), self.size)
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.size)
+
+    def update_color(self):
+        self.color = (0, (123 - (self.star_type * random.randrange(22, 30))), (132 - (self.star_type * random.randrange(10, 30))))
+
+    def update_position(self):
+        self.x = self.x + (self.star_type * self.size)/2
+        # self.x = self.x + self.star_type * (4-self.size) #Explosion
+        self.y = self.y + self.star_type/2
+        # Jeżeli wyjdzie po za ekran
+        if self.x > WIDTH:
+            self.x = self.x - WIDTH
+        if self.y > HEIGHT:
+            self.y = self.y - HEIGHT
 
     @staticmethod
     def show_xy():
@@ -46,14 +59,14 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
-    # WIN.fill((0, 0, 0))
     li_test = []
     for i in range(1, 500):
         li_test.append(Star())
     Star.show_xy()
 
     while run:
-        clock.tick(10)
+        clock.tick(12)
+
         WIN.fill((0, 0, 0))
         pygame.display.update()
 
@@ -61,10 +74,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        for star in li_test:
-            star.draw(WIN)
 
-        #polar2.draw(WIN)
+        for star in li_test:
+            star.update_color()
+            star.update_position()
+            star.draw(WIN)  #polar2.draw(WIN)
         pygame.display.update()
 
     pygame.quit()
